b1093de744d00f6e9b31fb4a39ebadeaecb125e0
bcuzbroa <bcuzbroa@proton.me>
2025-12-23 19:46:57 +0100

    Deleted challenges_answers

--- challenges_answers/challenge1.rs
+++
@@ -1,9 +0,0 @@
-// OWNERSHIP
-
-fn challenge_2(input : String) -> String {
-
-    //Todo : Add the " processed" suffixe to the input
-    //Is the '+' operator symetric ?
-
-    input + " processed"
-}
